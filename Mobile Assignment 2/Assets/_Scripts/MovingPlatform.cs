beff67edf7c7e04615f8015088dc72781a33c140
Blair-White <blairjwhite@hotmail.com>
2020-11-21 02:22:11 -0500

    All platforms working now. next menu buttons scene transition. then done.

--- Mobile Assignment 2/Assets/_Scripts/MovingPlatform.cs
+++ Mobile Assignment 2/Assets/_Scripts/MovingPlatform.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
+    private GameObject mPlayer;
+    private Transform PointA, PointB;
+    private Vector3 posA, posB, oldPos, moveVector;
+    private enum States { left, right};
+    private States State = States.right;
+    public bool isRiding;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mPlayer = GameObject.FindGameObjectWithTag("Player");
+        PointA = this.transform;
+        PointB = this.transform.GetChild(0);
+        posA = PointA.transform.position;
+        posB = PointB.transform.position;
+        oldPos = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        switch (State)
+        {
+            case States.left:
+                this.transform.position = Vector3.MoveTowards(this.transform.position, posA, 3.0f * Time.deltaTime);
+                if(Vector3.Distance(this.transform.position, posA) < 0.01f)
+                {
+                    State = States.right;
+                }
+                break;
+            case States.right:
+                this.transform.position = Vector3.MoveTowards(this.transform.position, posB, 3.0f * Time.deltaTime);
+                if (Vector3.Distance(this.transform.position, posB) < 0.01f)
+                {
+                    State = States.left;
+                }
+                break;
+            default:
+                break;
+        }
+
+        oldPos = this.transform.position;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+   
     }
 }
--- Mobile Assignment 2/Assets/_Scripts/SpikePlatform.cs
+++ Mobile Assignment 2/Assets/_Scripts/SpikePlatform.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class SpikePlatform : MonoBehaviour
 {
+    private enum States { Idle, Flipping}
+    [SerializeField]
+    private States State = States.Idle;
+    private bool isSafe;
+    private int flipCount;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,61 @@ public class SpikePlatform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        switch (State)
+        {
+            case States.Idle:
+                flipCount++;
+                if (flipCount > 1200)
+                {
+                    State = States.Flipping;
+                    flipCount = 0;
+                }
+                break;
+            case States.Flipping:
+                
+                if (!isSafe)
+                {
+
+                    if (this.transform.rotation.eulerAngles.z < 180)
+                    {
+                        transform.Rotate(0.0f, 0.0f, 0.1f, Space.Self);
+                    }
+                    if(this.transform.rotation.eulerAngles.z >= 180)
+                    {
+                        this.transform.rotation = new Quaternion(0.0f, 0.0f, 180, 0.0f);
+                        isSafe = true;
+                        State = States.Idle;
+                    }
+                }
+
+                if (isSafe)
+                {
+                    if (this.transform.rotation.eulerAngles.z > 0)
+                    {
+                        transform.Rotate(0.0f, 0.0f, -0.1f, Space.Self);
+                    }
+                    if (this.transform.rotation.eulerAngles.z <=0)
+                    {
+                        this.transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+                        isSafe = false;
+                        State = States.Idle;
+                    }
+                }
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            if(!this.isSafe)
+            {
+                col.gameObject.SendMessage("Die");
+            }
+        }
     }
 }
